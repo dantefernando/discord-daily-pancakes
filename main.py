12f13d3af45c664d66093738435cf7bbc3a9853c
Dante Fernando <62758801+dantefernando@users.noreply.github.com>
2020-04-05 00:04:29 +0200

    v1.0 Initial Commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from account import password
 
 class DiscordBot:
     def __init__(self, username1, password1):
-        self.driver = webdriver.Chrome("C:/Users/Fernpe2/Downloads/chromedriver.exe") # CHANGE THIS VALUE TO DIR OF
+        self.driver = webdriver.Chrome("C:/Users/Fernpe2/Downloads/chromedriver.exe")  # CHANGE THIS VALUE TO DIR OF
         # WEBDRIVER!!!
         self.driver.get(
             "https://discordapp.com/channels/626436080672964628/671821869707427851")  # Opens up Specific Discord
